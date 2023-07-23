e673a961ecb827a3ce5071b46c00fa24fd1785f3
Mpho Vincent Makgwe <mphomakgwe4@gmail.com>
2023-07-23 22:10:19 +0200

    long story

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,78 @@
+import React from 'react';
 
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton'
-import SortIcon from '@mui/icons-material/Sort';
-import SearchIcon from '@mui/icons-material/Search';
-import MenuIcon from '@mui/icons-material/Menu';
+const headerStyles = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  backgroundColor: '#2196f3',
+  color: '#fff',
+  padding: '1rem',
+};
 
-
+const menuButtonStyles = {
+  marginRight: '1rem',
+};
 
 const Header = () => {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
-          
-        </IconButton>
-        <Typography variant="h4" sx={{ flexGrow: 1 }}>
-          Pod-Lax with Vincent
-        </Typography>
-        <IconButton color="inherit" aria-label="search">
-          <SearchIcon />
-        </IconButton>
-        <SortIcon />
-      </Toolbar>
-    </AppBar>
+    <div style={headerStyles}>
+      <div style={menuButtonStyles}>
+        {/* You can add your menu button icon here */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          width="24"
+          height="24"
+        >
+          {/* Add your menu icon SVG path here */}
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h16m-7 6h7"
+          />
+        </svg>
+      </div>
+      <h4>Pod-Lax with Vincent</h4>
+      <div>
+        {/* You can add your search icon here */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          width="24"
+          height="24"
+        >
+          {/* Add your search icon SVG path here */}
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 6.5c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5 6.5-2.91 6.5-6.5-2.91-6.5-6.5-6.5zm0 0l-3 3"
+          />
+        </svg>
+        {/* You can add your sort icon here */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          width="24"
+          height="24"
+        >
+          {/* Add your sort icon SVG path here */}
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h8m-8 6h16"
+          />
+        </svg>
+      </div>
+    </div>
   );
 };
 
