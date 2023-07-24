2b355fd45a272b3ccae31a4b0fc39a94a73d2f7a
Mpho Vincent Makgwe <mphomakgwe4@gmail.com>
2023-07-24 15:43:12 +0200

    Debugging

--- src/components/Episode.jsx
+++ src/components/Episode.jsx
@@ -1,10 +1,12 @@
 // Episode.js
+
 const Episode = ({ episode, onPlay }) => {
   return (
     <div className="episode">
       <h3>{episode.title}</h3>
       <p>{episode.description}</p>
       <p>Release Date: {new Date(episode.releaseDate).toLocaleDateString()}</p>
+      <button onClick={() => onPlay(episode)}>Play Episode</button>
     </div>
   );
 };
