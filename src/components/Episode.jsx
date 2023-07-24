9ab299ebeac89cc29ff5d869797651f629f23bec
Mpho Vincent Makgwe <mphomakgwe4@gmail.com>
2023-07-24 16:53:50 +0200

    doner

--- src/components/Episode.jsx
+++ src/components/Episode.jsx
@@ -6,7 +6,6 @@ const Episode = ({ episode, onPlay }) => {
       <h3>{episode.title}</h3>
       <p>{episode.description}</p>
       <p>Release Date: {new Date(episode.releaseDate).toLocaleDateString()}</p>
-      <button onClick={() => onPlay(episode)}>Play Episode</button>
     </div>
   );
 };
